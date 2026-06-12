d9006805dd6a4b8e834397cb6383e946a00d0975
Ricardo <landinezrichard@gmail.com>
2026-06-12 17:19:52 -0500

    fix: importar Context en componente CalorieTracker

--- src/context/ActivityContext.tsx
+++ src/context/ActivityContext.tsx
@@ -1,6 +1,7 @@
 import {
   createContext,
   useReducer,
+  useMemo,
   type ReactNode,
   type Dispatch,
 } from "react";
@@ -18,19 +19,49 @@ type ActivityProviderProps = {
 type ActivityContextProps = {
   state: ActivityState;
   dispatch: Dispatch<ActivityActions>;
+  caloriesConsumed: number;
+  caloriesBurned: number;
+  netCalories: number;
 };
 
 export const ActivityContext = createContext<ActivityContextProps>(null!);
 
 export const ActivityProvider = ({ children }: ActivityProviderProps) => {
-	
   const [state, dispatch] = useReducer(activityReducer, initialState);
+
+  const caloriesConsumed = useMemo(
+    () =>
+      state.activities.reduce(
+        (total, activity) =>
+          activity.category === 1 ? total + activity.calories : total,
+        0,
+      ),
+    [state.activities],
+  );
+
+  const caloriesBurned = useMemo(
+    () =>
+      state.activities.reduce(
+        (total, activity) =>
+          activity.category === 2 ? total + activity.calories : total,
+        0,
+      ),
+    [state.activities],
+  );
+
+  const netCalories = useMemo(
+    () => caloriesConsumed - caloriesBurned,
+    [caloriesConsumed, caloriesBurned],
+  );
 
   return (
     <ActivityContext.Provider
       value={{
         state,
         dispatch,
+        caloriesConsumed,
+        caloriesBurned,
+        netCalories,
       }}
     >
       {children}
